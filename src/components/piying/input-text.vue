2de47627b9e135989756b9c19554d302d926ae63
chen <wszgrcy@gmail.com>
2025-07-24 23:21:44 +0800

    init

---
+++ src/components/piying/input-text.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useControlValueAccessor } from '@piying/view-vue'
+import { vModelText } from 'vue'
+import { vModelDynamic } from 'vue'
+
+const { cva, value, disabled, valueChange, touchedChange } = useControlValueAccessor()
+defineExpose({ cva })
+</script>
+<template>
+  <input
+    type="text"
+    class="input"
+    v-model-dynamic="value"
+    :onUpdate:modelValue="valueChange"
+    v-bind:disabled="disabled"
+    @blur="touchedChange"
+  />
+</template>
+
+<style></style>
