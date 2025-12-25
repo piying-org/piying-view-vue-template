7397debd21964e7038ad5f7a96cbd322857dc419
chen <wszgrcy@gmail.com>
2025-12-25 20:42:14 +0800

    update 2.0.7

--- src/components/define.ts
+++ src/components/define.ts
@@ -6,12 +6,13 @@ import FieldsetGroup from './piying/group/fieldset.vue'
 import LabelWrapper from './piying/wrapper/label-wrapper.vue'
 import ValidatorWrapper from './piying/wrapper/validator-wrapper.vue'
 import type { PiViewConfig } from '@piying/view-vue'
+import { actions } from '@piying/view-core'
 export const fieldConfig = {
   types: {
-    string: { type: InputText, wrappers: ['label'] },
-    number: { type: InputNumber, wrappers: ['label'] },
+    string: { type: InputText, actions: [actions.wrappers.set(['label'])] },
+    number: { type: InputNumber, actions: [actions.wrappers.set(['label'])] },
     radio: { type: InputRadio },
-    boolean: { type: InputCheckbox, wrappers: ['label'] },
+    boolean: { type: InputCheckbox, actions: [actions.wrappers.set(['label'])] },
     fieldset: { type: FieldsetGroup },
     formHelper: { type: () => import('./piying/form-helper.vue') },
   },
